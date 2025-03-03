76f6cb20ce756b54efed9ddb792801c87100acf8
Katsiaryna Andrabaila <androbailoe@gmail.com>
2025-03-03 18:46:10 +0100

    feat: add footer

--- src/widgets/footer/index.tsx
+++ src/widgets/footer/index.tsx
@@ -1,3 +1,9 @@
 export const Footer = () => {
-    return <></>;
+    return (
+        <footer className="absolute bottom-0 left-0 right-0 bg-gray-100">
+            <div className="h-auto w-9/10 mx-auto my-2 text-right">
+                <a href="https://github.com/Katsiaryna-Andrabaila">© Katsiaryna Andrabaila</a>
+            </div>
+        </footer>
+    );
 };
--- src/widgets/header/index.tsx
+++ src/widgets/header/index.tsx
@@ -34,7 +34,7 @@ export const Header = () => {
     return (
         <header ref={ref} className={clsx([
             'fixed top-0 left-0 right-0 z-1 transition duration-300 ease',
-            scrollBg && 'bg-gray-800',
+            scrollBg && 'bg-gray-100',
         ])}>
             <div className="flex justify-between align-center h-auto w-9/10 mx-auto my-4">
                 <Link to="/" className="font-[Alex_Brush] text-4xl">
