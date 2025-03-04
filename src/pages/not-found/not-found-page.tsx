c23750a10b1b2840cefcd009f2ee364159a62f6f
Katsiaryna Andrabaila <androbailoe@gmail.com>
2025-03-04 18:22:54 +0100

    feat: add 404 page

--- src/pages/not-found/not-found-page.tsx
+++ src/pages/not-found/not-found-page.tsx
@@ -1,3 +1,8 @@
 export const NotFoundPage = () => {
-    return <>404 page</>;
+    return (
+        <div className="flex flex-col items-center gap-4 w-9/10 h-full mx-auto my-20">
+            <p className="text-[15rem] text-[#808080] dark:text-gray-200">404</p>
+            <h1 className="text-gray-700">Page not found!</h1>
+        </div>
+    );
 };
