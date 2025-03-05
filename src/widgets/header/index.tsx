31fe616feccf163ce5c08cc72e0b03dc19372f1a
Katsiaryna Andrabaila <androbailoe@gmail.com>
2025-03-05 11:39:56 +0100

    fix: fix header styles

--- src/widgets/header/index.tsx
+++ src/widgets/header/index.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useMatch, useResolvedPath } from 'react-router-dom';
 import clsx from 'clsx';
 
 import { SvgIcon, ThemeSwitcher } from '@/shared/ui';
@@ -7,6 +7,8 @@ import { SvgIcon, ThemeSwitcher } from '@/shared/ui';
 export const Header = () => {
     const ref = useRef<HTMLElement | null>(null);
     const [scrollBg, setScrollBg] = useState(false);
+    const resolved = useResolvedPath("/favorites");
+    const match = useMatch({ path: resolved.pathname, end: true });
 
     useEffect(() => {
         const handleScroll = (elRef: React.RefObject<HTMLElement | null>) => {
@@ -27,14 +29,18 @@ export const Header = () => {
     return (
         <header ref={ref} className={clsx([
             'fixed top-0 left-0 right-0 z-1 transition duration-300 ease',
-            scrollBg && 'bg-gray-100 dark:bg-gray-400 opacity-90',
+            scrollBg && 'bg-gray-100 dark:bg-gray-400 opacity-95',
         ])}>
             <div className="flex justify-between items-center h-auto w-9/10 mx-auto my-3">
                 <Link to="/" className="font-[Alex_Brush] text-4xl">
                     Gallery
                 </Link>
                 <Link to="/favorites" className="flex items-center justify-center flex-1 max-w-fit ml-auto mr-4">
-                    <SvgIcon name="icon-fav" size={32} color="grey" />
+                    <SvgIcon
+                        name="icon-fav"
+                        size={32}
+                        className={clsx(['text-grey-200', match && 'text-red-400'])}
+                    />
                 </Link>
                 <ThemeSwitcher />
             </div>
